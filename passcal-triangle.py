d7cad184c4e6864dbaa53657a736641fa3636e36
Mykhailo-Dobryden <dobrydenmykhail@gmail.com>
2023-07-12 22:15:17 +0300

    triangle of Pascalle

---
+++ passcal-triangle.py
@@ -0,0 +1,14 @@
+n = int(input("Enter a number for building of Passcal triangle: "))
+triangle = []
+
+for i in range(n):
+    triangle.append([1] + [0] * n)
+
+for i in range(1, n):
+    for j in range(1, n+1):
+        triangle[i][j] = triangle[i-1][j] + triangle[i-1][j-1]
+
+for i in range(0, n):
+    for j in range(0, i+1):
+        print(triangle[i][j], end="\t")
+    print()
